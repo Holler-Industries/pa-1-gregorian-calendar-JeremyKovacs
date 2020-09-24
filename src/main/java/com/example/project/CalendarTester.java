71c53c0810e3bb5f47b35463cd4496023cdbe870
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2020-09-24 15:52:17 -0400

    I finished the project. The part about my birthday will not always be correct because the calendar changes but my birthday stays in the same place. The program is correct on the date 9/24/2020 because that is the date that I wrote this part of the program.

--- src/main/java/com/example/project/CalendarTester.java
+++ src/main/java/com/example/project/CalendarTester.java
@@ -19,8 +19,8 @@ public class CalendarTester {
 
         //Call the .get method on myCal using Calendar.DAY_OF_WEEK as the argument
 
-        System.out.println("myCal Day of Week: " + myCal.get(Calendar.DAY_OF_WEEK));
-        System.out.println("Expected: 4");     //Note: 1 = Sunday, 2 = Monday, etc.
+        //System.out.println("myCal Day of Week: " + myCal.get(Calendar.DAY_OF_WEEK));
+        //System.out.println("Expected: 4");     //Note: 1 = Sunday, 2 = Monday, etc.
 
 
         //Write similar tests for month, year and day of month.
@@ -33,6 +33,42 @@ public class CalendarTester {
 
         //Finally, find what day of the week is 10,000 days after your 2020 birthday. Note, you may have to play around
         //with adding or subtracting days to get to your birthday date (hmm, how can you subtract days?)
+
+        int Month = myCal.get(Calendar.MONTH) + 1;
+        int Date = myCal.get(Calendar.DAY_OF_MONTH);
+        int Year = myCal.get(Calendar.YEAR);
+
+        System.out.println("Today's date is: " + Month + "/" + Date + "/" + Year + ".");
+
+
+        myCal.add(Calendar.DAY_OF_YEAR,100);
+
+        int Month2 = myCal.get(Calendar.MONTH) + 1;
+        int Date2 = myCal.get(Calendar.DAY_OF_MONTH);
+        int Year2 = myCal.get(Calendar.YEAR);
+        int Day2 = myCal.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println("100 days from today the date will be: " + Month2 + "/" + Date2 + "/" + Year2 + " and the day of the week will be: " + Day2 + ".");
+
+        myCal.add(Calendar.DAY_OF_YEAR,-237);
+
+        int Month3 = myCal.get(Calendar.MONTH) + 1;
+        int Date3 = myCal.get(Calendar.DAY_OF_MONTH);
+        int Day3 = myCal.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println("My birthday is: " + Month3 + "/" + Date3 + " and this year was on the day of the week of " + Day3 + ".");
+
+        //This will not always be correct because the calendar is changing but my birthday is in the same place.
+        //This is correct as of 9/24/2020.
+        //The next print statement will also only be correct on 9/24/2020 for the same reason.
+
+        myCal.add(Calendar.DAY_OF_YEAR, 10000);
+
+        int Month4 = myCal.get(Calendar.MONTH) + 1;
+        int Date4 = myCal.get(Calendar.DAY_OF_MONTH);
+        int Year4 = myCal.get(Calendar.YEAR);
+
+        System.out.println("10,000 days from my last birthday the date will be: " + Month4 + "/" + Date4 + "/" + Year4 + ".");
 
 
     }
